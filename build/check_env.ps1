8bdf7a52d489e4f6ab3673e3848252ac01129682
Mike Monaco <mamonaco1973@gmail.com>
2025-01-01 15:58:53 -0500

    tweak env script

--- build/check_env.ps1
+++ build/check_env.ps1
@@ -1,5 +1,4 @@
-
-Write-Output "NOTE: Validating that required commands are found in your PATH."
+Write-Host "NOTE: Validating that required commands are found in your PATH." -ForegroundColor Green
 $commands = @("aws", "packer", "terraform")
 $all_found = $true
 
@@ -7,23 +6,23 @@ foreach ($cmd in $commands) {
     if (-not (Get-Command $cmd -ErrorAction SilentlyContinue)) {
         Write-Error "ERROR: $cmd is not found in the current PATH."
         $all_found = $false
-    } else {
-        Write-Output "NOTE: $cmd is found in the current PATH."
+    } else { 
+        Write-Host "NOTE: $cmd is found in the current PATH." -ForegroundColor Green
     }
 }
 
 if ($all_found) {
-    Write-Output "NOTE: All required commands are available."
+    Write-Host "NOTE: All required commands are available." -ForegroundColor Green
 } else {
-    Write-Error "ERROR: One or more commands are missing."
+    Write-Error "ERROR: One or more commands are missing." 
     exit 1
 }
 
-Write-Output "NOTE: Checking AWS CLI connection."
+Write-Host "NOTE: Checking AWS CLI connection." -ForegroundColor Green
 try {
     $account = aws sts get-caller-identity --query "Account" --output text
-    Write-Output "NOTE: Successfully logged in to AWS."
+    Write-Host "NOTE: Successfully logged into AWS." -ForegroundColor Green
 } catch {
-    Write-Error "ERROR: Failed to connect to AWS. Please check your credentials and environment variables."
+    Write-Error "ERROR: Failed to connect to AWS. Please check your credentials and environment variables." 
     exit 1
 }
